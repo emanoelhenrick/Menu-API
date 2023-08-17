df081d4215bfa7ca07ceda4ca97149119955d960
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-08-17 18:01:30 -0300

    chore: add EqualsAndHash from lombok

--- src/main/java/com/emhk/menu/menuapi/domain/models/Product.java
+++ src/main/java/com/emhk/menu/menuapi/domain/models/Product.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Product {
   
   private Long id;
   private String name;
   private String description;
   private BigDecimal price;
-  private List<ProductCategory> categories;
+  private ProductCategory category;
   private String imageUrl;
   private String ingredients;
 
--- src/main/java/com/emhk/menu/menuapi/domain/models/ProductCategory.java
+++ src/main/java/com/emhk/menu/menuapi/domain/models/ProductCategory.java
@@ -3,21 +3,17 @@ package com.emhk.menu.menuapi.domain.models;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Menu {
+@Entity
+public class ProductCategory {
   
-  @Id
-  @EqualsAndHashCode.Include
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  private String name;
+  private String description;
 
   private List<Product> products;
 
