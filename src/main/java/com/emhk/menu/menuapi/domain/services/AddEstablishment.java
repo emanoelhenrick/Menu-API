f591c8354028a519147c81648f49476b9b08644d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-08-23 16:01:44 -0300

    refactor: move add establishment service to a folder

--- src/main/java/com/emhk/menu/menuapi/domain/services/AddEstablishment.java
+++
@@ -1,7 +0,0 @@
-package com.emhk.menu.menuapi.domain.services;
-
-import com.emhk.menu.menuapi.domain.models.Establishment;
-
-public interface AddEstablishment {
-  public Establishment add(Establishment establishment);
-}
