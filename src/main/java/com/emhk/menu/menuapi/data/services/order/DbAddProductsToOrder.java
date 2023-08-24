e2e6b6e38464ace579c2ccdbfc751a3da76565ee
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-08-24 18:20:38 -0300

    feat: implements AddProductsToOrder

---
+++ src/main/java/com/emhk/menu/menuapi/data/services/order/DbAddProductsToOrder.java
@@ -0,0 +1,26 @@
+package com.emhk.menu.menuapi.data.services.order;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emhk.menu.menuapi.domain.models.Order;
+import com.emhk.menu.menuapi.domain.models.Product;
+import com.emhk.menu.menuapi.domain.repository.OrderRepository;
+import com.emhk.menu.menuapi.domain.services.order.AddProductsToOrder;
+
+@Service
+public class DbAddProductsToOrder implements AddProductsToOrder {
+
+  @Autowired
+  private OrderRepository repository;
+
+  @Override
+  public Order add(Long OrderId, List<Product> products) {
+    var order = repository.findById(OrderId).orElseThrow();
+    products.forEach(product -> order.getProducts().add(product));
+    return repository.save(order);
+  }
+  
+}
--- src/main/java/com/emhk/menu/menuapi/domain/services/order/AddProductsToOrder.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/order/AddProductsToOrder.java
@@ -6,5 +6,5 @@ import com.emhk.menu.menuapi.domain.models.Order;
 import com.emhk.menu.menuapi.domain.models.Product;
 
 public interface AddProductsToOrder {
-  public Order add(List<Product> products);
+  public Order add(Long OrderId, List<Product> products);
 }
