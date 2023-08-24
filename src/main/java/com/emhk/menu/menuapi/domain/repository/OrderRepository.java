636e6f1220aa26b5748e35aa803f3ef583d5aa27
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-08-24 18:24:42 -0300

    feat: add findByExternal(uuid) method

--- src/main/java/com/emhk/menu/menuapi/domain/repository/OrderRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/OrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.emhk.menu.menuapi.domain.models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-  
+  Order findByExternalId(String uuid);
 }
