05cb1fa1e9a6081207979fc1532b3cb7a59fc3d8
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-08-24 18:16:29 -0300

    feat: create AddProductsToOrder interface

---
+++ src/main/java/com/emhk/menu/menuapi/domain/services/order/AddProductsToOrder.java
@@ -0,0 +1,10 @@
+package com.emhk.menu.menuapi.domain.services.order;
+
+import java.util.List;
+
+import com.emhk.menu.menuapi.domain.models.Order;
+import com.emhk.menu.menuapi.domain.models.Product;
+
+public interface AddProductsToOrder {
+  public Order add(List<Product> products);
+}
