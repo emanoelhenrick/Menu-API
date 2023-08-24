4067c490aebffeb8d683ac036b291ecb1209e697
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-08-24 18:08:39 -0300

    feat: add update order status interface service

---
+++ src/main/java/com/emhk/menu/menuapi/domain/services/order/UpdateOrderStatus.java
@@ -0,0 +1,7 @@
+package com.emhk.menu.menuapi.domain.services.order;
+
+import com.emhk.menu.menuapi.domain.models.OrderStatus;
+
+public interface UpdateOrderStatus {
+  public void update(Long id, OrderStatus status);
+}
