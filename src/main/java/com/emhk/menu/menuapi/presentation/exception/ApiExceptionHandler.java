5767517e03e4b45862b4ac0dfcc16abadb650e84
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 15:52:49 -0300

    chore: add @Nullable to body

--- src/main/java/com/emhk/menu/menuapi/presentation/exception/ApiExceptionHandler.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/exception/ApiExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,7 +16,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(
-		Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request
+		Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request
 	) {
 		if (body == null) {
 			body = ProblemDetails.builder()
