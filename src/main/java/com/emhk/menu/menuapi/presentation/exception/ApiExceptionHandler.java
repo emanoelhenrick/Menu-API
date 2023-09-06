b940f0c2098c7c43f938360ea2a401737b858663
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 16:07:08 -0300

    feat: ensure EntityNotFoundException returns a ProblemDetails at ExceptionHandler

--- src/main/java/com/emhk/menu/menuapi/presentation/exception/ApiExceptionHandler.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/exception/ApiExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.emhk.menu.menuapi.presentation.exception;
 
+import com.emhk.menu.menuapi.domain.exceptions.EntityNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -14,22 +16,32 @@ import java.time.OffsetDateTime;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@Override
+	@ExceptionHandler
+	public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest req) {
+		var status = HttpStatus.NOT_FOUND;
+		var detail = ex.getMessage();
+		var problemType = ProblemType.RESOURCE_NOT_FOUND;
+		var body = makeProblemBuilder(status, problemType, detail)
+			.userMessage(detail)
+			.build();
+		return handleExceptionInternal(ex, body, new HttpHeaders(), status, req);
+	}
+
 	protected ResponseEntity<Object> handleExceptionInternal(
-		Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request
+		Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request
 	) {
 		if (body == null) {
 			body = ProblemDetails.builder()
-				.status(statusCode.value())
-				.title(ex.getMessage())
+				.status(status.value())
+				.title(status.getReasonPhrase())
 				.build();
 		} else if (body instanceof String) {
 			body = ProblemDetails.builder()
-				.status(statusCode.value())
+				.status(status.value())
 				.title((String) body)
 				.build();
 		}
-		return new ResponseEntity<>(body, headers, statusCode);
+		return new ResponseEntity<>(body, headers, status);
 	}
 
 	private ProblemDetails.ProblemDetailsBuilder makeProblemBuilder(
