f46acbfadec3171dadb0bc7b08ce4e3aa71875f9
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 19:27:28 -0300

    refactor: fix InUseEntityException

--- src/main/java/com/emhk/menu/menuapi/domain/exceptions/InUseEntityException.java
+++ src/main/java/com/emhk/menu/menuapi/domain/exceptions/InUseEntityException.java
@@ -1,7 +1,7 @@
 package com.emhk.menu.menuapi.domain.exceptions;
 
-public class InUseEntityException extends Exception {
-  InUseEntityException(String value) {
-    super(value);
+public class InUseEntityException extends BusinessException {
+  public InUseEntityException(String id) {
+    super("the entity with id " + id + " is in use");
   }
 }
