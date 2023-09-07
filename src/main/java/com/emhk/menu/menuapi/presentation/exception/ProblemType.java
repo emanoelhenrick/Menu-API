08f78cf6bdec0231e5a1d0fb50178959009680d1
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 22:07:25 -0300

    feat: add in use entity and entity not available

--- src/main/java/com/emhk/menu/menuapi/presentation/exception/ProblemType.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/exception/ProblemType.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-  RESOURCE_NOT_FOUND("Resource not found.");
+  RESOURCE_NOT_FOUND("Resource not found."),
+  IN_USE_ENTITY("In use entity."),
+  ENTITY_NOT_AVAILABLE("Entity not available.");
 
   ProblemType(String title) {}
 }
