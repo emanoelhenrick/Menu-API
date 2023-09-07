d09e2d5dafcce33e6acbf40e94110f1be0566fda
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 22:06:51 -0300

    refactor: extend from EntityNotAvailable

--- src/main/java/com/emhk/menu/menuapi/domain/exceptions/product/ProductNotAvailable.java
+++ src/main/java/com/emhk/menu/menuapi/domain/exceptions/product/ProductNotAvailable.java
@@ -1,8 +1,8 @@
 package com.emhk.menu.menuapi.domain.exceptions.product;
 
-import com.emhk.menu.menuapi.domain.exceptions.BusinessException;
+import com.emhk.menu.menuapi.domain.exceptions.EntityNotAvailable;
 
-public class ProductNotAvailable extends BusinessException {
+public class ProductNotAvailable extends EntityNotAvailable {
 	public ProductNotAvailable(String id) {
 		super("product with id " + id + "is inactive");
 	}
