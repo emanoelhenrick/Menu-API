8580d8a59ddd6c6e261d55abb9a257c3250ee337
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 22:08:05 -0300

    refactor: replace id to use reason text

--- src/main/java/com/emhk/menu/menuapi/domain/exceptions/InUseEntityException.java
+++ src/main/java/com/emhk/menu/menuapi/domain/exceptions/InUseEntityException.java
@@ -1,7 +1,7 @@
 package com.emhk.menu.menuapi.domain.exceptions;
 
 public class InUseEntityException extends BusinessException {
-  public InUseEntityException(String id) {
-    super("the entity with id " + id + " is in use");
+  public InUseEntityException(String reason) {
+    super(reason);
   }
 }
