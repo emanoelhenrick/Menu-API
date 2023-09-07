f6e210a3b0c074f97045894638758de90aa65d76
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-07 18:01:15 -0300

    feat: add findByUsername method

--- src/main/java/com/emhk/menu/menuapi/domain/repository/UserRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.emhk.menu.menuapi.domain.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.emhk.menu.menuapi.domain.models.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-  
+  public Optional<User> findByUsername(String username);
 }
