6ecd9f2a7566caac7b8ef66e91ffb52287ffe9f0
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-07 18:01:40 -0300

    chore: replace userId to use username instead

--- src/main/java/com/emhk/menu/menuapi/domain/services/order/FindOrdersByUser.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/order/FindOrdersByUser.java
@@ -5,5 +5,5 @@ import com.emhk.menu.menuapi.domain.models.Order;
 import java.util.List;
 
 public interface FindOrdersByUser {
-  List<Order> find(String userId);
+  List<Order> find(String username);
 }
