73f6ed19540d657cb646dae8e433d2f7c80f48b8
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-06 22:44:22 -0300

    chore: add id to order output

--- src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/order/output/OrderOutput.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/order/output/OrderOutput.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Setter
 public class OrderOutput {
 
+	private String id;
 	private List<ProductOrderOutput> products;
 	private BigDecimal totalPrice;
 	private OrderStatus status;
