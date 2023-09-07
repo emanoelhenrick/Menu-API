dd9050c27b964aa880ed91160a4ce1753dbef6bd
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-07 10:29:34 -0300

    feat: add invalid data type

--- src/main/java/com/emhk/menu/menuapi/presentation/exception/ProblemType.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/exception/ProblemType.java
@@ -8,6 +8,7 @@ public enum ProblemType {
   IN_USE_ENTITY("In use entity."),
   INTERNAL_SERVER_ERROR("Internal server error."),
   BAD_REQUEST("Bad request."),
+  INVALID_DATA("Invalid data."),
   ENTITY_NOT_AVAILABLE("Entity not available.");
 
   ProblemType(String title) {}
