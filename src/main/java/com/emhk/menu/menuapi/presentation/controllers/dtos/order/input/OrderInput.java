44e0af2517a2ff2810d0ea5f4bae32bfc4dc7ed1
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-07 18:11:35 -0300

    refactor: replace user id to use username instead

--- src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/order/input/OrderInput.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/order/input/OrderInput.java
@@ -1,9 +1,8 @@
 package com.emhk.menu.menuapi.presentation.controllers.dtos.order.input;
 
 import com.emhk.menu.menuapi.presentation.controllers.dtos.product.input.ProductOrderInput;
-import com.emhk.menu.menuapi.presentation.controllers.dtos.user.input.UserIdInput;
+import com.emhk.menu.menuapi.presentation.controllers.dtos.user.input.UserUsernameInput;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +14,7 @@ import java.util.List;
 public class OrderInput {
 
 	@Valid
-	private UserIdInput customer;
+	private UserUsernameInput customer;
 
 	@NotNull
 	private List<ProductOrderInput> products;
