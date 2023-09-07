d55fb7c475594f72c481e3022ad37821ef864c63
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-07 09:28:43 -0300

    feat: add bad request

--- src/main/java/com/emhk/menu/menuapi/presentation/exception/ProblemType.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/exception/ProblemType.java
@@ -7,6 +7,7 @@ public enum ProblemType {
   RESOURCE_NOT_FOUND("Resource not found."),
   IN_USE_ENTITY("In use entity."),
   INTERNAL_SERVER_ERROR("Internal server error."),
+  BAD_REQUEST("Bad request."),
   ENTITY_NOT_AVAILABLE("Entity not available.");
 
   ProblemType(String title) {}
