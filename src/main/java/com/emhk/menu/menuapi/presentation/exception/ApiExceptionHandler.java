773b607764c07faf3600f1764e6b3847d9722f5a
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-07 10:30:55 -0300

    feat: implement handleMethodArgumentNotValid

--- src/main/java/com/emhk/menu/menuapi/presentation/exception/ApiExceptionHandler.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/exception/ApiExceptionHandler.java
@@ -4,10 +4,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 import static com.emhk.menu.menuapi.presentation.exception.ExceptionHandlerHelper.*;
 
@@ -23,6 +27,31 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			.userMessage("An unexpected system error occurred, please try again and if the problem persists, contact support.")
 			.build();
 		return makeResponseEntity(ex, body, new HttpHeaders(), status, req);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request
+	) {
+    var status = HttpStatus.resolve(statusCode.value());
+		var problemType = ProblemType.INVALID_DATA;
+		var detail = "One or more fields are invalid. Fill in correctly and try again.";
+
+		List<ProblemDetails.Field> fields = ex.getBindingResult()
+			.getFieldErrors().stream()
+			.map(fieldError -> {
+				return ProblemDetails.Field.builder()
+					.name(fieldError.getField())
+					.userMessage(fieldError.getDefaultMessage())
+					.build();
+			}).toList();
+
+    var body = makeProblemBuilder(status, problemType, detail)
+      .userMessage(detail)
+      .fields(fields)
+      .build();
+
+		return handleExceptionInternal(ex, body, headers, status, request);
 	}
 
 	@Override
