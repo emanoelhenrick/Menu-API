ad461babc83a52a9342fa5e60de3af741372582e
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-09 16:38:12 -0300

    feat: create OrderStatusInput dto

---
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/order/input/OrderStatusInput.java
@@ -0,0 +1,18 @@
+package com.emhk.menu.menuapi.presentation.controllers.dtos.order.input;
+
+import com.emhk.menu.menuapi.core.validation.EnumValidator;
+import com.emhk.menu.menuapi.domain.models.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderStatusInput {
+
+  @EnumValidator(
+    enumClazz = OrderStatus.class,
+    message = "must enter a valid status"
+  )
+  private String status;
+
+}
