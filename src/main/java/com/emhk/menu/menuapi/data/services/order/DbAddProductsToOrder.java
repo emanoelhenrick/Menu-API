82b05eddc0a96bfd7716cc5e54592d7c8e76481e
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-09 17:19:02 -0300

    chore: add product repository and calc total price

--- src/main/java/com/emhk/menu/menuapi/data/services/order/DbAddProductsToOrder.java
+++ src/main/java/com/emhk/menu/menuapi/data/services/order/DbAddProductsToOrder.java
@@ -1,10 +1,14 @@
 package com.emhk.menu.menuapi.data.services.order;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
 import com.emhk.menu.menuapi.domain.exceptions.order.OrderNotFoundException;
+import com.emhk.menu.menuapi.domain.exceptions.product.ProductNotAvailable;
+import com.emhk.menu.menuapi.domain.exceptions.product.ProductNotFoundException;
 import com.emhk.menu.menuapi.domain.models.ProductOrder;
+import com.emhk.menu.menuapi.domain.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import com.emhk.menu.menuapi.domain.models.Order;
@@ -17,9 +21,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class DbAddProductsToOrder implements AddProductsToOrder {
 
   private final OrderRepository orderRepository;
+  private final ProductRepository productRepository;
 
-  DbAddProductsToOrder(OrderRepository orderRepository) {
+  DbAddProductsToOrder(OrderRepository orderRepository, ProductRepository productRepository) {
     this.orderRepository = orderRepository;
+    this.productRepository = productRepository;
   }
 
   @Override
@@ -27,7 +33,24 @@ public class DbAddProductsToOrder implements AddProductsToOrder {
     var order = orderRepository
       .findById(UUID.fromString(orderUUID))
       .orElseThrow(() -> new OrderNotFoundException(orderUUID));
+
     products.forEach(product -> order.getProducts().add(product));
+
+    var totalPrice = BigDecimal.ZERO;
+    for (ProductOrder productOrder : order.getProducts()) {
+      var productId = productOrder.getProductId();
+      var product = productRepository.findById(UUID.fromString(productId))
+        .orElseThrow(() -> new ProductNotFoundException(productId));
+
+      if (!product.getActive()) throw new ProductNotAvailable(productId);
+
+      productOrder.setUnitPrice(product.getPrice());
+      var productTotalPrice = product.getPrice()
+        .multiply(BigDecimal.valueOf(productOrder.getQuantity()));
+      totalPrice = totalPrice.add(productTotalPrice);
+    }
+    order.setTotalPrice(totalPrice);
+
     return orderRepository.save(order);
   }
   
