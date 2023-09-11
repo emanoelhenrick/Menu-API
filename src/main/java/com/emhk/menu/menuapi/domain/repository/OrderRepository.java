fa2923e61abcd4a180e37ddec16d3074300e5bfd
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-11 14:36:19 -0300

    feat: ensure that findAllBycustomerUsername returns a Page of Orders

--- src/main/java/com/emhk/menu/menuapi/domain/repository/OrderRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/OrderRepository.java
@@ -2,10 +2,12 @@ package com.emhk.menu.menuapi.domain.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.emhk.menu.menuapi.domain.models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-
+  Page<Order> findAllByCustomerUsername(String username, Pageable pageable);
 }
