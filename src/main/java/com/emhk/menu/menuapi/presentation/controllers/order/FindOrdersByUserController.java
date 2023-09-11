3a0b01498b9f4403b462a75bdedddf1741e501ad
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-11 14:39:18 -0300

    refactor: ensure that controller receive a pageable and returns a page of orders

--- src/main/java/com/emhk/menu/menuapi/presentation/controllers/order/FindOrdersByUserController.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/order/FindOrdersByUserController.java
@@ -4,15 +4,16 @@ import com.emhk.menu.menuapi.domain.services.order.FindOrdersByUser;
 import com.emhk.menu.menuapi.presentation.controllers.dtos.order.output.OrderOutput;
 import com.emhk.menu.menuapi.presentation.controllers.dtos.order.output.assembler.OrderAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/user/{userId}/orders")
+@RequestMapping("/user/{username}/orders")
 public class FindOrdersByUserController {
 
   @Autowired
@@ -22,9 +23,10 @@ public class FindOrdersByUserController {
   private OrderAssembler assembler;
 
   @GetMapping
-  public List<OrderOutput> findOrders(@PathVariable String userId) {
-    var orders = findOrdersByUser.find(userId);
-    return assembler.toDTOCollection(orders);
+  public Page<OrderOutput> findOrders(@PathVariable String username, Pageable pageable) {
+    var ordersPage = findOrdersByUser.find(username, pageable);
+    var orders = assembler.toDTOCollection(ordersPage.getContent());
+    return new PageImpl<OrderOutput>(orders, pageable, ordersPage.getTotalElements());
   }
 
 }
