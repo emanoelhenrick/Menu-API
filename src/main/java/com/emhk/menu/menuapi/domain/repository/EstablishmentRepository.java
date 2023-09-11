ba0699281186936d71afaee4c0c6e22401ef230d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-11 14:45:53 -0300

    refactor: ensure findAllByOwnerUsername returns a page

--- src/main/java/com/emhk/menu/menuapi/domain/repository/EstablishmentRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/EstablishmentRepository.java
@@ -1,13 +1,13 @@
 package com.emhk.menu.menuapi.domain.repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.emhk.menu.menuapi.domain.models.Establishment;
 
 public interface EstablishmentRepository extends JpaRepository<Establishment, UUID> {
-  Optional<List<Establishment>> findAllByOwnerUsername(String username);
+  Page<Establishment> findAllByOwnerUsername(String username, Pageable pageable);
 }
