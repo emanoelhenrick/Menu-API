c621269b749fb1516ed290b0a4d381e5690223ed
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 16:12:33 -0300

    feat: implement AddEstablishmentTable service

---
+++ src/main/java/com/emhk/menu/menuapi/data/services/establishmentTable/DbAddEstablishmentTable.java
@@ -0,0 +1,30 @@
+package com.emhk.menu.menuapi.data.services.establishmentTable;
+
+import com.emhk.menu.menuapi.domain.exceptions.establishment.EstablishmentNotFoundException;
+import com.emhk.menu.menuapi.domain.models.EstablishmentTable;
+import com.emhk.menu.menuapi.domain.repository.EstablishmentRepository;
+import com.emhk.menu.menuapi.domain.repository.EstablishmentTableRepository;
+import com.emhk.menu.menuapi.domain.services.establishmentTable.AddEstablishmentTable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DbAddEstablishmentTable implements AddEstablishmentTable {
+
+  private final EstablishmentTableRepository establishmentTableRepository;
+  private final EstablishmentRepository establishmentRepository;
+
+  DbAddEstablishmentTable(
+    EstablishmentTableRepository establishmentTableRepository,
+    EstablishmentRepository establishmentRepository
+  ) {
+    this.establishmentTableRepository = establishmentTableRepository;
+    this.establishmentRepository = establishmentRepository;
+  }
+  @Override
+  public EstablishmentTable add(EstablishmentTable establishmentTable) {
+    var establishmentId = establishmentTable.getEstablishment().getId();
+    establishmentRepository.findById(establishmentId)
+      .orElseThrow(() -> new EstablishmentNotFoundException(establishmentId.toString()));
+    return establishmentTableRepository.save(establishmentTable);
+  }
+}
