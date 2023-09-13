e37cc4c80caa2aae52131fef0dbe4d9d879aa784
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:23:08 -0300

    refactor: ensure fndAllByCustomerUsername returns a list

--- src/main/java/com/emhk/menu/menuapi/domain/repository/OrderRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/OrderRepository.java
@@ -1,13 +1,13 @@
 package com.emhk.menu.menuapi.domain.repository;
 
+import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.emhk.menu.menuapi.domain.models.Order;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-  Page<Order> findAllByCustomerUsername(String username, Pageable pageable);
+  List<Order> findAllByCustomerUsername(String username, Pageable pageable);
 }
