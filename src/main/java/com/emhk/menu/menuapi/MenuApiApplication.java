5ec410df53771472f314331c36e0fa52443e4631
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:33:14 -0300

    feat: ensure the application uses UTC timezone

--- src/main/java/com/emhk/menu/menuapi/MenuApiApplication.java
+++ src/main/java/com/emhk/menu/menuapi/MenuApiApplication.java
@@ -3,10 +3,13 @@ package com.emhk.menu.menuapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MenuApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(MenuApiApplication.class, args);
 	}
 
