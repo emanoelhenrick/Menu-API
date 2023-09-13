16db0c4f869ca717df24908c4627e9111325748d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:52:16 -0300

    feat: add EstablishmentTableInput

--- src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/establishmentTable/input/EstablishmentTableInput.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/establishmentTable/input/EstablishmentTableInput.java
@@ -1,5 +1,10 @@
 package com.emhk.menu.menuapi.presentation.controllers.dtos.establishmentTable.input;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EstablishmentTableInput {
 
   private String id;
