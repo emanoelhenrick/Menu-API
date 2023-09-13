afba0484ebdf4ba7496e3b654b52b48e970aa207
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:23:54 -0300

    refactor: ensure FindOrdersByUser returns a list

--- src/main/java/com/emhk/menu/menuapi/domain/services/order/FindOrdersByUser.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/order/FindOrdersByUser.java
@@ -1,9 +1,10 @@
 package com.emhk.menu.menuapi.domain.services.order;
 
 import com.emhk.menu.menuapi.domain.models.Order;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface FindOrdersByUser {
-  Page<Order> find(String username, Pageable pageable);
+  List<Order> find(String username, Pageable pageable);
 }
