351497dd4f63f92e12c32783a32517ec6babc57d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 16:01:38 -0300

    feat: create AddEstablishmentTable service interface

---
+++ src/main/java/com/emhk/menu/menuapi/domain/services/establishmentTable/AddEstablishmentTable.java
@@ -0,0 +1,7 @@
+package com.emhk.menu.menuapi.domain.services.establishmentTable;
+
+import com.emhk.menu.menuapi.domain.models.EstablishmentTable;
+
+public interface AddEstablishmentTable {
+  EstablishmentTable add(EstablishmentTable establishmentTable);
+}
