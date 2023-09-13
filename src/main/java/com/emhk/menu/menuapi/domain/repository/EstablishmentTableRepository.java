c78789a4b4892310c53b6d72b15fd4b5952a2df4
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 16:12:09 -0300

    fix: replace entity class

--- src/main/java/com/emhk/menu/menuapi/domain/repository/EstablishmentTableRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/EstablishmentTableRepository.java
@@ -1,6 +1,7 @@
 package com.emhk.menu.menuapi.domain.repository;
 
+import com.emhk.menu.menuapi.domain.models.EstablishmentTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EstablishmentTableRepository extends JpaRepository<EstablishmentTableRepository, Long> {
+public interface EstablishmentTableRepository extends JpaRepository<EstablishmentTable, Long> {
 }
