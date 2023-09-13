c9c816e2e4e3a761c08acb3f5867bcb1443ae57c
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:58:31 -0300

    feat: create EstablishmentTableRepository

---
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/EstablishmentTableRepository.java
@@ -0,0 +1,6 @@
+package com.emhk.menu.menuapi.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstablishmentTableRepository extends JpaRepository<EstablishmentTableRepository, Long> {
+}
