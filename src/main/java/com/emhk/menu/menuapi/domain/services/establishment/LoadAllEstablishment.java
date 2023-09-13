d55eda0ab4f2f427bdf52f3afb9602c40df719de
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:25:03 -0300

    refactor: ensure loadAllByOwner returns a list

--- src/main/java/com/emhk/menu/menuapi/domain/services/establishment/LoadAllEstablishment.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/establishment/LoadAllEstablishment.java
@@ -3,9 +3,8 @@ package com.emhk.menu.menuapi.domain.services.establishment;
 import java.util.List;
 
 import com.emhk.menu.menuapi.domain.models.Establishment;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface LoadAllEstablishment {
-  public Page<Establishment> loadAllByOwner(String username, Pageable pageable);
+  List<Establishment> loadAllByOwner(String username, Pageable pageable);
 }
