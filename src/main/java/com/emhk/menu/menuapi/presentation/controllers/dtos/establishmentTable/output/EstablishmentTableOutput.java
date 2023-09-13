1d7460f1f1ecef18668974494c99cd38b0c26936
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-13 15:46:34 -0300

    feat: create EstablishmentTableOutput

---
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/establishmentTable/output/EstablishmentTableOutput.java
@@ -0,0 +1,17 @@
+package com.emhk.menu.menuapi.presentation.controllers.dtos.establishmentTable.output;
+
+import com.emhk.menu.menuapi.presentation.controllers.dtos.establishment.output.EstablishmentShortOutput;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EstablishmentTableOutput {
+
+  private String id;
+  private int number;
+  private EstablishmentShortOutput establishment;
+  private Boolean isBusy;
+  private Boolean active;
+
+}
