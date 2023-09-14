ff207f4a285ce5efa61158c2b65cf0280ce919bd
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-14 16:57:18 -0300

    refactor: add establishmentId arg

--- src/main/java/com/emhk/menu/menuapi/domain/services/establishmentTable/AddEstablishmentTable.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/establishmentTable/AddEstablishmentTable.java
@@ -3,5 +3,5 @@ package com.emhk.menu.menuapi.domain.services.establishmentTable;
 import com.emhk.menu.menuapi.domain.models.EstablishmentTable;
 
 public interface AddEstablishmentTable {
-  EstablishmentTable add(EstablishmentTable establishmentTable);
+  EstablishmentTable add(EstablishmentTable establishmentTable, String establishmentId);
 }
