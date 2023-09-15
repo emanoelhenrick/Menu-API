5105479ca3df995d79a43aeec8c014afccbb6775
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-15 08:53:11 -0300

    chore: add getter and setter lombok anno

--- src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/productImage/input/productImage.java
+++ src/main/java/com/emhk/menu/menuapi/presentation/controllers/dtos/productImage/input/productImage.java
@@ -2,9 +2,13 @@ package com.emhk.menu.menuapi.presentation.controllers.dtos.productImage.input;
 
 import com.emhk.menu.menuapi.config.validation.FileContentType;
 import com.emhk.menu.menuapi.config.validation.FileSize;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+@Getter
+@Setter
 public class productImage {
 
   @FileSize(max = "1MB")
