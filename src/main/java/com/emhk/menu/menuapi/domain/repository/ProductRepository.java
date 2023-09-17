e6d12425759b974f2a77a0d275c2bad2a4aeb103
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-17 20:30:35 -0300

    feat: implements findImageById on repository

--- src/main/java/com/emhk/menu/menuapi/domain/repository/ProductRepository.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/ProductRepository.java
@@ -1,12 +1,19 @@
 package com.emhk.menu.menuapi.domain.repository;
 
+import com.emhk.menu.menuapi.domain.models.ProductImage;
 import com.emhk.menu.menuapi.domain.repository.customQueries.ProductRepositoryQueries;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.emhk.menu.menuapi.domain.models.Product;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface ProductRepository extends JpaRepository<Product, UUID>, ProductRepositoryQueries {
 
+  @Query("select f from ProductImage f join f.product p" +
+    "where p.restaurant.id = :restaurantId and p.product.id = :productId")
+  Optional<ProductImage> findImageById(UUID restaurantId, UUID productId);
 }
