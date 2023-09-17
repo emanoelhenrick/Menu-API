4e2ab6b56108d20f332e76beb0d9c73dc67652d8
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-17 20:29:45 -0300

    feat: add delete product image method to product repository

--- src/main/java/com/emhk/menu/menuapi/domain/repository/customQueries/ProductRepositoryQueries.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/customQueries/ProductRepositoryQueries.java
@@ -3,5 +3,8 @@ package com.emhk.menu.menuapi.domain.repository.customQueries;
 import com.emhk.menu.menuapi.domain.models.ProductImage;
 
 public interface ProductRepositoryQueries {
+
   ProductImage saveProductImage(ProductImage productImage);
+  void delete(ProductImage productImage);
+
 }
