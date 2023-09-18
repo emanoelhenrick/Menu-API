9e060d63a84bb17223e61ecdc7b4e513ea20c7e5
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-18 20:18:20 -0300

    chore: add inputStream to arguments

--- src/main/java/com/emhk/menu/menuapi/domain/services/product/productImage/AddProductImage.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/product/productImage/AddProductImage.java
@@ -2,6 +2,8 @@ package com.emhk.menu.menuapi.domain.services.product.productImage;
 
 import com.emhk.menu.menuapi.domain.models.ProductImage;
 
+import java.io.InputStream;
+
 public interface AddProductImage {
-  ProductImage add(ProductImage productImage, String productId);
+  ProductImage add(ProductImage productImage, String productId, InputStream inputStream);
 }
