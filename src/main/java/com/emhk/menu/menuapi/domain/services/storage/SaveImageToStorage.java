ed4eacf7c0322520e171b9dbfa03de807962095f
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-19 17:30:04 -0300

    feat: add remove method and default method replace

--- src/main/java/com/emhk/menu/menuapi/domain/services/storage/SaveImageToStorage.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/storage/SaveImageToStorage.java
@@ -8,7 +8,18 @@ import java.io.InputStream;
 import java.util.UUID;
 
 public interface SaveImageToStorage {
+
   void save(NewImage newImage) throws IOException;
+
+  void remove(String filename);
+
+  default void replace(String oldFilename, NewImage newImage) throws IOException {
+    save(newImage);
+
+    if (oldFilename != null) {
+      remove(oldFilename);
+    }
+  }
 
   default String generateFilename(String filename) {
     return UUID.randomUUID().toString() + filename;
