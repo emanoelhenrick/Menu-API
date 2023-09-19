3704d3dfd9f3c2a6530d6870b8a0dd03c404b88b
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-19 17:08:20 -0300

    feat: add generateFilename method to prefix UUID at filename

--- src/main/java/com/emhk/menu/menuapi/domain/services/storage/SaveImageToStorage.java
+++ src/main/java/com/emhk/menu/menuapi/domain/services/storage/SaveImageToStorage.java
@@ -5,9 +5,14 @@ import lombok.Getter;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 public interface SaveImageToStorage {
   void save(NewImage newImage) throws IOException;
+
+  default String generateFilename(String filename) {
+    return UUID.randomUUID().toString() + filename;
+  }
 
   @Builder
   @Getter
