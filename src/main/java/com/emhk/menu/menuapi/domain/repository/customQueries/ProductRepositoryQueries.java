a5ad54ab75a26c57aa92bd71e4b6f0e94286b255
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-23 18:15:45 -0300

    refactor: ensure findProductImage returns an Optional of ProductImage

--- src/main/java/com/emhk/menu/menuapi/domain/repository/customQueries/ProductRepositoryQueries.java
+++ src/main/java/com/emhk/menu/menuapi/domain/repository/customQueries/ProductRepositoryQueries.java
@@ -2,10 +2,12 @@ package com.emhk.menu.menuapi.domain.repository.customQueries;
 
 import com.emhk.menu.menuapi.domain.models.ProductImage;
 
+import java.util.Optional;
+
 public interface ProductRepositoryQueries {
 
   ProductImage saveProductImage(ProductImage productImage);
-  ProductImage findProductImage(String productId);
+  Optional<ProductImage> findProductImage(String productId);
   void deleteProductImage(ProductImage productImage);
 
 }
