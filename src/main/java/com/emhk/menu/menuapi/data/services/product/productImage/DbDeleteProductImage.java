0a1c383e432c52a0fcfd4135b8628585daff3ec6
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-09-27 21:27:55 -0300

    feat: add DeleteProductImage service

---
+++ src/main/java/com/emhk/menu/menuapi/data/services/product/productImage/DbDeleteProductImage.java
@@ -0,0 +1,31 @@
+package com.emhk.menu.menuapi.data.services.product.productImage;
+
+import com.emhk.menu.menuapi.domain.exceptions.product.productImage.ProductImageNotFoundException;
+import com.emhk.menu.menuapi.domain.repository.ProductRepository;
+import com.emhk.menu.menuapi.domain.services.product.productImage.DeleteProductImage;
+import com.emhk.menu.menuapi.domain.services.storage.ImageStorageService;
+import org.springframework.transaction.annotation.Transactional;
+
+public class DbDeleteProductImage implements DeleteProductImage {
+
+  private final ImageStorageService imageStorageService;
+  private final ProductRepository productRepository;
+
+  DbDeleteProductImage(
+    ImageStorageService imageStorageService,
+    ProductRepository productRepository
+  ) {
+    this.imageStorageService = imageStorageService;
+    this.productRepository = productRepository;
+  }
+
+  @Override
+  @Transactional
+  public void delete(String productId) {
+    var productImage = productRepository.findProductImage(productId)
+      .orElseThrow(() -> new ProductImageNotFoundException(productId));
+    productRepository.deleteProductImage(productImage);
+    productRepository.flush();
+    imageStorageService.remove(productImage.getFileName());
+  }
+}
---
+++ src/main/java/com/emhk/menu/menuapi/domain/services/product/productImage/DeleteProductImage.java
@@ -0,0 +1,5 @@
+package com.emhk.menu.menuapi.domain.services.product.productImage;
+
+public interface DeleteProductImage {
+  void delete(String productId);
+}
